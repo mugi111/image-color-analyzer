9ac2c9c875f65aac527c3519447cd9e638c378df
mugi <m.111_t.1230_nittc@outlook.com>
2020-10-08 06:20:36 +0900

    http hello world

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/list", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World from Go.")
+}
