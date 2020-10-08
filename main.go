56c73f2f65b0d13e47f7db30cdf2c0721a31e456
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-10-08 12:59:46 +0900

    add upload

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ import (
 
 func main() {
 	http.HandleFunc("/list", handler)
+	http.HandleFunc("/upload", upload)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World from Go.")
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Invalid Method")
+		return
+	}
 }
