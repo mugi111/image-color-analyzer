f93891dc60d5889f5680ef9a8df1ac483dbeeea3
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-11-13 12:54:25 +0900

    upload image

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -21,4 +23,29 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Invalid Method")
 		return
 	}
+
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	defer file.Close()
+
+	out, err := os.Create(header.Filename)
+	if err != nil {
+		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+		return
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+
+	fmt.Fprintf(w, "File uploaded successfully: ")
+	fmt.Fprintf(w, header.Filename)
 }
