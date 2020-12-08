123d25945f3cd718f853006e1155ae573d100178
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-08 18:25:42 +0900

    add .vercel

--- main.go
+++ main.go
@@ -1,60 +1,60 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"os"
+// )
 
-func main() {
-	http.HandleFunc("/list", handler)
-	http.HandleFunc("/upload", upload)
-	http.ListenAndServe(":8080", nil)
-}
+// func main() {
+// 	http.HandleFunc("/list", handler)
+// 	http.HandleFunc("/upload", upload)
+// 	http.ListenAndServe(":8080", nil)
+// }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World from Go.")
-}
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Hello World from Go.")
+// }
 
-func upload(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Invalid Method")
-	}
+// func upload(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodPost {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		fmt.Fprint(w, "Invalid Method")
+// 	}
 
-	file, header, err := r.FormFile("file")
+// 	file, header, err := r.FormFile("file")
 
-	if err != nil {
-		fmt.Fprintln(w, err)
-		return
-	}
+// 	if err != nil {
+// 		fmt.Fprintln(w, err)
+// 		return
+// 	}
 
-	fileHeader := make([]byte, 512)
+// 	fileHeader := make([]byte, 512)
 
-	if _, err := file.Read(fileHeader); err != nil {
-		fmt.Fprintf(w, "err read")
-		return
-	}
+// 	if _, err := file.Read(fileHeader); err != nil {
+// 		fmt.Fprintf(w, "err read")
+// 		return
+// 	}
 
-	file.Seek(0, 0)
+// 	file.Seek(0, 0)
 
-	defer file.Close()
+// 	defer file.Close()
 
-	out, err := os.Create(header.Filename)
-	if err != nil {
-		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
-		return
-	}
+// 	out, err := os.Create(header.Filename)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+// 		return
+// 	}
 
-	defer out.Close()
+// 	defer out.Close()
 
-	_, err = io.Copy(out, file)
-	if err != nil {
-		fmt.Fprintln(w, err)
-	}
+// 	_, err = io.Copy(out, file)
+// 	if err != nil {
+// 		fmt.Fprintln(w, err)
+// 	}
 
-	fmt.Fprintf(w, "File uploaded successfully: ")
-	fmt.Fprintf(w, header.Filename)
-	fmt.Fprintf(w, "MIME: %#v\n", http.DetectContentType(fileHeader))
-}
+// 	fmt.Fprintf(w, "File uploaded successfully: ")
+// 	fmt.Fprintf(w, header.Filename)
+// 	fmt.Fprintf(w, "MIME: %#v\n", http.DetectContentType(fileHeader))
+// }
