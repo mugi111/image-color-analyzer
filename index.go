3e76b9345d17a5f84b053990631d110db8f86618
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-08 17:57:58 +0900

    add vercel config

---
+++ index.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+// Handler main handler
+func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>image-color-analyzer</h1>")
+}
